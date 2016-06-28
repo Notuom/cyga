2b6ac45573aa7668154a7eb84ed922d003cfbd43
Guillaume Chevalier <guillaumechevalier@Monsieur-MacBook.local>
2016-06-28 11:20:56 -0400

    preloading des acronymes

--- app/database/DatabaseManager.js
+++ app/database/DatabaseManager.js
@@ -12,8 +12,26 @@ var DatabaseManager = function DatabaseManager() {
 
 };
 
+DatabaseManager.prototype.getAcronyms = function getAcronyms(callback) {
+  var client = new pg.Client(config.databaseUrl);
+  client.connect();
+  client.query('SELECT acronym, definition FROM log515_cyga.acronym;', function(err, result) {
+      if(err)
+        callback(err, null);
+      else
+        callback(null, result.rows);
+    });
+};
+
 DatabaseManager.prototype.listTables = function listTables() {
-  pg.connect(config.databaseUrl, function (err, client) {
+  var client = new pg.Client(config.databaseUrl);
+  client.connect();
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function (row) {
+      console.log(JSON.stringify(row));
+    });
+  /*pg.connect(config.databaseUrl, function (err, client) {
     if (err) {
       console.log(err);
       throw err;
@@ -25,7 +43,7 @@ DatabaseManager.prototype.listTables = function listTables() {
       .on('row', function (row) {
         console.log(JSON.stringify(row));
       });
-  });
+  });*/
 };
 
 module.exports = DatabaseManager;
