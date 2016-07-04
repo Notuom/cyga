7afa029fc5e6752d7f1d038dba9c3351fe6063ea
Guillaume Chevalier <guillaumechevalier@Monsieur-MacBook.local>
2016-07-04 19:07:12 -0400

    script de seeding des acronymes.

---
+++ seeding_acronym.sql
@@ -0,0 +1,17 @@
+SET SCHEMA 'log515_cyga';
+
+INSERT INTO acronym(acronym, definition)
+VALUES
+  ('AA', 'alcooliques anonymes'),
+  ('AG', 'assemblée générale'),
+  ('FIQ', 'fédération des infirmières du québec'),
+  ('HLM', 'habitation loyer modique'),
+  ('MTQ', 'ministère du transport du québec'),
+  ('OSBL', 'organisme sans but lucratif'),
+  ('RH', 'ressources humaines'),
+  ('PMBOK', 'project management book of knowledge'),
+  ('ITIL', 'information technology infrastructure library'),
+  ('ARP', 'address resolution protocol'),
+  ('FC', 'focres canadiennes'),
+  ('MRC', 'municipalité régionale de comté'),
+  ('QG', 'quartier général');
