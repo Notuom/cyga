e7929e9bfd2c44b0e0fd3c19dda7258b05d04f53
Kevin Jacques <kevin.g.jacques@gmail.com>
2016-07-10 19:21:46 -0400

    Protection on admin pages to only admin

--- app/routing/adminRoutes.js
+++ app/routing/adminRoutes.js
@@ -6,10 +6,16 @@ router.get('/', function(req, res) {
   res.redirect('/acronym');
 });
 
-router.get('/acronym', function(req, res) {
+router.get('/acronym',function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated() && req.user.type == 'admin') {
+    return next();
+  }
+  req.session.error = 'Your are not authorize to view this page!';
+  res.redirect('/users/account');
+}, function(req, res) {
   var db = new DatabaseManager();
   db.getAcronyms(function (acronyms) {
-    res.render('adminAcronym', { acronyms: acronyms});
+    res.render('adminAcronym', { acronyms: acronyms, user : req.user});
   });
 });
 
