7afd033ecc2dcf34c78ba3bca80cf6bdcf4a13d6
Karl Thibault <thibault.karl@gmail.com>
2018-05-11 12:51:16 -0400

    Update config.js

--- app/config.js
+++ app/config.js
@@ -4,6 +4,6 @@ var config = {};
 config.port = process.env.PORT
   || 8080;
 config.databaseUrl = process.env.DATABASE_URL
-  || "postgres://***REMOVED***";
+  || "postgres://YOUR_LOCAL_DATABASE_URL";
 
 module.exports = config;
